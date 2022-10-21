f36d831cbf7fa46fce3fd6486691ccda93deca2b
19002248 <l.gis.cas@gmail.com>
2022-10-20 19:39:40 -0500

    Actualización usuarios

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,  
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports:[
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    
   ]
 })
 export class MaterialModule { }
